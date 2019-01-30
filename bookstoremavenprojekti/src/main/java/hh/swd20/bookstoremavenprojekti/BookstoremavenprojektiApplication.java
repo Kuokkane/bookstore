646c298315d67e79577eedaa8463d09ad32627d0
Kuokkane <tonikuokkanen@hotmail.com>
2019-01-30 10:55:03 +0200

    Projekti aloitettu

---
+++ bookstoremavenprojekti/src/main/java/hh/swd20/bookstoremavenprojekti/BookstoremavenprojektiApplication.java
@@ -0,0 +1,14 @@
+package hh.swd20.bookstoremavenprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremavenprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremavenprojektiApplication.class, args);
+	}
+
+}
+
---
+++ bookstoremavenprojekti/src/test/java/hh/swd20/bookstoremavenprojekti/BookstoremavenprojektiApplicationTests.java
@@ -0,0 +1,17 @@
+package hh.swd20.bookstoremavenprojekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstoremavenprojektiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
