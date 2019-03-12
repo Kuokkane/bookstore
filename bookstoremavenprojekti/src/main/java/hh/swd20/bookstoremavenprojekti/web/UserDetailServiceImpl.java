adf56759b93c2fae31164e5ec4ef906e02353e17
Kuokkane <tonikuokkanen@hotmail.com>
2019-03-12 18:43:52 +0200

    securing the web application 2

---
+++ bookstoremavenprojekti/src/main/java/hh/swd20/bookstoremavenprojekti/web/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package hh.swd20.bookstoremavenprojekti.web;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import hh.swd20.bookstoremavenprojekti.domain.User;
+import hh.swd20.bookstoremavenprojekti.domain.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+	
+	private final UserRepository urepository;
+	
+	@Autowired
+	public UserDetailServiceImpl(UserRepository urepository ) {
+		this.urepository = urepository;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String user) throws UsernameNotFoundException {
+		
+		User curruser = urepository.findByUsername(user);
+		UserDetails userDetail = new org.springframework.security.core.userdetails.User(user, curruser.getPasswordHash(),
+			AuthorityUtils.createAuthorityList(curruser.getRole()));
+		return userDetail;
+	}
+}
